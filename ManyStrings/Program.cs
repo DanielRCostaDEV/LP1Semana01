3cdd80726181f6441c68b3ba459f885c755d5686
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-23 12:40:40 +0000

    Strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -12,6 +12,15 @@ namespace ManyStrings
             string c = "This is a copyright \u03A9";
             Console.WriteLine(c);
 
+            string m = "One" + "string";
+            Console.WriteLine(m);
+
+            int x = 5;
+            int z = 10;
+            string f = $"{x} plus {z} equals to {x + z}";
+            Console.WriteLine(f);
+            
+
         }
     }
 }
