93543bb2deae25a03ddc21ead13f4e54dd4a6370
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-23 09:39:24 +0000

    Print Integer Variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte sb = 8;
+            Console.WriteLine($"The value of sbyte is {sb} ");
+            
+            short sh = 16;
+            Console.WriteLine($"The value of short is {sh} ");
+            
+            int inte = 42527;
+            Console.WriteLine($"The value of int is {inte} ");
+            
+            long lo = 42528;
+            Console.WriteLine($"The value of long is {lo}" + 'L');
+            
+            byte by = 8;
+            Console.WriteLine($"The value of byte is {by} ");
+            
+            ushort bugs = 0;
+            Console.WriteLine($"The value of ushort is {bugs} ");
+            
+            uint ui = 32;
+            Console.WriteLine($"The value of uint is {ui}" + 'U');
+            
+            ulong ul = 33;
+            Console.WriteLine($"The value of ulong is {ul}" + 'U' + 'L');
+            
+            char ch = 'N';
+            Console.WriteLine($"The value of char is {ch} ");
+            
+        }
+    }
+}
