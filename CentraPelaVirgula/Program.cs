2400095f62f426dbe790c170ff7e2a825dd15aaf
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-23 14:48:31 +0000

    Ask user for a input

---
+++ CentraPelaVirgula/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CentraPelaVirgula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write 4 real numbers:");
+            string 1 = Console.ReadLine();
+            string 2 = Console.ReadLine();
+            string 3 = Console.ReadLine();
+            string 4 = Console.ReadLine();
+
+        }
+    }
+}
