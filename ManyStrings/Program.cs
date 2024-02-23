5544ed087b8f1d022e21129de417f0962fc662cd
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-23 11:17:22 +0000

    Print Strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "This is a tab: \t ";
+            Console.WriteLine(s);
+
+            string c = "This is a copyright \u03A9";
+            Console.WriteLine(c);
+
+        }
+    }
+}
