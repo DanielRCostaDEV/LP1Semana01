8b54c5fbd201d3330f10e724b66f20600ad8ef2e
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-23 14:31:16 +0000

    String to Int

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,18 +6,10 @@ namespace AskForInput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Indica-me 4 numeros reais");
-            string a = Console.ReadLine();
-            string b = Console.ReadLine();
-            string c = Console.ReadLine();
-            string d = Console.ReadLine();
-
-            double num1 = double.Parse(a);
-            double num2 = double.Parse(b);
-            double num3 = double.Parse(c);
-            double num4 = double.Parse(d);
-
-            Console.WriteLine($"{num1,6:f2}\n{num2,6:f2} \n{num3,6:f2}\n{num4,6:f2}");
+            Console.WriteLine("Please enter a whole number: ");
+            string tint = Console.ReadLine();
+            int sint = int.Parse(tint);
+            
         }
     }
 }
