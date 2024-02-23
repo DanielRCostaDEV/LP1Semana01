3e095ace6ed8b9990fe0bea0033030fd33dd7a27
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-23 15:00:11 +0000

    Print the numbers pushed to right side

--- CentraPelaVirgula/Program.cs
+++ CentraPelaVirgula/Program.cs
@@ -7,11 +7,20 @@ namespace CentraPelaVirgula
         static void Main(string[] args)
         {
             Console.WriteLine("Write 4 real numbers:");
-            string 1 = Console.ReadLine();
-            string 2 = Console.ReadLine();
-            string 3 = Console.ReadLine();
-            string 4 = Console.ReadLine();
+            string n1 = Console.ReadLine();
+            double num1 = double.Parse(n1);
+            
+            string n2 = Console.ReadLine();
+            double num2 = double.Parse(n2);
+            
+            string n3 = Console.ReadLine();
+            double num3 = double.Parse(n3);
+            
+            string n4 = Console.ReadLine();
+            double num4 = double.Parse(n4);
 
+            //Print the numbers
+            Console.WriteLine($"{num1,2:f2}\n{num2,2:f2} \n{num3,2:f2}\n{num4,2:f2}");
         }
     }
 }
