691ff41d1a659f1f612122ca75d9daffb4d27cf8
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-23 14:39:07 +0000

    Real number and string to float

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,10 +6,18 @@ namespace AskForInput
     {
         static void Main(string[] args)
         {
+            //User input (Whole n.) and string to int
             Console.WriteLine("Please enter a whole number: ");
             string tint = Console.ReadLine();
             int sint = int.Parse(tint);
             
+            //User input (Real n.) and string to float
+            Console.WriteLine("Please enter a real number: ");
+            string tf = Console.ReadLine();
+            float stf = float.Parse(tf);
+            
+            float total = sint + stf;
+            Console.WriteLine($"The total of {sint} + {sint} is {total}");
         }
     }
 }
