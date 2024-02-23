a2077d2485e59d0977a359d8867994b281daa111
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-23 09:03:51 +0000

    Added ///

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
 namespace MeuPrograma
-{
+{/// <An approach to take notes>
+/// An approach to take notes and write about what the program is
+/// </and write about what the program is>
     class Program
     {
         static void Main(string[] args)
