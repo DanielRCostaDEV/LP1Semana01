c3577f0af8c543f04fb541e222ce3101b27b31a6
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-23 13:27:08 +0000

    Format Strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,6 +6,18 @@ namespace ManyStrings
     {
         static void Main(string[] args)
         {
+            double xx = 1.23456;
+            int ii = 19;
+            string nr = $"Real number of xx with two decimals:{xx,5:f2}";
+            Console.WriteLine(nr);
+            string pr = $"Percentage of xx with one decimal: {xx:p1}";
+            Console.WriteLine(pr);
+
+            string hx = $"The hexadecimal value of {ii} is {ii:x}";
+            Console.WriteLine(hx);
+            string eu = $"The currency value of {ii} is {ii:c}";
+            Console.WriteLine(eu);
+            
             string s = "This is a tab: \t ";
             Console.WriteLine(s);
 
